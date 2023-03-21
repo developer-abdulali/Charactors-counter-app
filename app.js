febf23e5fb0fb7fa419d12a2480fa516859a00ee
Abdul Ali <103456792+developer-abdulali@users.noreply.github.com>
2023-03-21 19:36:38 +0500

    Add files via upload
    Charactor counter app take input charactors and count the charactors and show total charactors length.

---
+++ app.js
@@ -0,0 +1,5 @@
+countword();
+function countwords(){
+    var numbers = document.getElementById('words').value.length;
+    document.getElementById('showdata').innerHTML = 'The total Charactors = ' + numbers;
+}
